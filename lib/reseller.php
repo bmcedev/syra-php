ec4aef04e44ff383ef326b09845f71764d801aac
Melissa Choy <mel@otilas.com.au>
2013-04-11 21:55:04 +1000

    include all classes

--- lib/reseller.php
+++ lib/reseller.php
@@ -4,7 +4,7 @@
  * Syra API Wrapper
  *
  * @author    Melissa Choy <mel@otilas.com.au>
- * @version   v0.0.1
+ * @version   v1.1
  * @copyright 2013 Otilas Pty Ltd
  */
 
--- syra.php
+++ syra.php
@@ -4,7 +4,7 @@
  * Syra API Wrapper
  *
  * @author    Melissa Choy <mel@otilas.com.au>
- * @version   v0.0.1
+ * @version   v1.1
  * @copyright 2013 Otilas Pty Ltd
  */
 
@@ -13,3 +13,8 @@
 $dir = realpath(dirname(__FILE__));
 
 include_once "{$dir}/syra/base.php";
+include_once "{$dir}/syra/contact.php";
+include_once "{$dir}/syra/domain.php";
+include_once "{$dir}/syra/host.php";
+include_once "{$dir}/syra/transfer.php";
+include_once "{$dir}/syra/reseller.php";
