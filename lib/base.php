7f5b15e720dda8c53e6e59c5f0d64838b77cffc3
Melissa Choy <mel@otilas.com.au>
2013-04-22 12:28:35 +1000

    cleanup formatting

--- lib/base.php
+++ lib/base.php
@@ -12,7 +12,7 @@ class SyraAPI {
   var $test_mode = false;
   private $reseller_api_soap_client;
   
-	function __construct($reseller_id=SYRA_RESELLER_ID, $api_key=SYRA_API_KEY, $test_mode=false) {	  
+  function __construct($reseller_id=SYRA_RESELLER_ID, $api_key=SYRA_API_KEY, $test_mode=false) {    
     if($test_mode!=true) {
       if ((defined('SYRA_TEST_MODE') && true == SYRA_TEST_MODE)) {
         $this->test_mode=true;
@@ -41,23 +41,23 @@ class SyraAPI {
 
     $this->reseller_api_soap_client = new SoapClient($wsdl_location, array('soap_version' => SOAP_1_2, 'cache_wsdl' => WSDL_CACHE_NONE));
     $this->reseller_api_soap_client->__setSoapHeaders(array($header));
-	}
+  }
 
-	protected function send_request($method, $data = null) {
-		$prepared_data = $data != null ? array($data) : array();
-		try {
-			$response = $this->reseller_api_soap_client->__soapCall($method, $prepared_data);
-		} catch (SoapFault $response) { }
-		
-		if (!is_soap_fault($response)) {
+  protected function send_request($method, $data = null) {
+    $prepared_data = $data != null ? array($data) : array();
+    try {
+      $response = $this->reseller_api_soap_client->__soapCall($method, $prepared_data);
+    } catch (SoapFault $response) { }
+    
+    if (!is_soap_fault($response)) {
       return $response->APIResponse;
     } else {
       throw new Exception('Error occurred while sending request: ' . $response->getMessage());
     }
-	}
-	
-	protected function api_errors($response) {
-	  $errors = (object) array('Errors' => $response->Errors);
-	  return $errors;
-	}
+  }
+  
+  protected function api_errors($response) {
+    $errors = (object) array('Errors' => $response->Errors);
+    return $errors;
+  }
 }
